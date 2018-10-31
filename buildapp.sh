e2396059946b715ec09adabf146c05e34858e74b
mnsaab@umich.edu <mnsaab@umich.edu>
2018-10-31 17:10:14 +0200

    yuhhh^4

--- buildapp.sh
+++ buildapp.sh
@@ -1,1 +1,3 @@
-docker build . -t walaintro
+docker build . -t walaintro --no-cache
+
+docker run walaintro:latest -e MYSQL_HOST=localhost -e MYSQL_PORT=3306
--- start-app.sh
+++ start-app.sh
@@ -1,1 +1,3 @@
-docker run -e MYSQL_ROOT_HOST=172.17.0.1 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=root -d mysql/mysql-server
+docker run -p3306:3306 -e MYSQL_ROOT_HOST=172.17.0.1 -e MYSQL_ROOT_PASSWORD=password -e MYSQL_USER=root -d mysql/mysql-server
+
+docker exec -it bdef75dfd47a mysql -uroot -ppassword
